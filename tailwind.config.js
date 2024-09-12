a3f9e41418128ee89baa3e9a0b23da13e86a9ca7
matheusWegner <matheus.christo@dbfactory.com.br>
2024-09-11 22:51:21 -0300

    ajustes css

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
       serif: ["Merriweather", "serif"],
+      verdana: ["Verdana"]
     },
     extend: {
       spacing: {
