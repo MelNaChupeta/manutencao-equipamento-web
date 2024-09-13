37996248fe21b14a5f6df3aff2a9b45df9e38f4d
matheusWegner <matheus.christo@dbfactory.com.br>
2024-09-13 19:45:39 -0300

    adjust css for mobile and add login simulation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, Renderer2, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faSignOut , faGear , faUser } from '@fortawesome/free-solid-svg-icons';
+import { faSignOut , faGear , faUser, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { User } from '../../models/user';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -10,26 +11,38 @@ import { faSignOut , faGear , faUser } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit, OnDestroy{
+  
   @ViewChild('menu') menu!: ElementRef;
   @ViewChild('menuButton') menuButton!: ElementRef;
   @ViewChild('navButton') navButton!: ElementRef;
   @ViewChild('nav') nav!: ElementRef;
   
-  faSignOut = faSignOut;
-  faGear = faGear;
-  faUser = faUser;
+  faSignOut:IconDefinition = faSignOut;
+  faGear:IconDefinition = faGear;
+  faUser:IconDefinition = faUser;
  
   menuActive:boolean = false;
   navActive:boolean = false;
+  logged:boolean = false;
   
-  constructor(private renderer: Renderer2) {}
+  user: User = {
+    id: 1,
+    name: 'Matheus Wegner',
+    email: 'matheus.wegner@example.com',
+    perfil: 'CLIENT',
+    isAuthenticated: false
+  };
+
+  constructor(private renderer: Renderer2) {
+    
+  }
   
   ngOnInit(): void {
      this.renderer.listen('body', 'click', (e) => {
-      if(e.target!==this.menu.nativeElement && !this.menu.nativeElement.contains(e.target)
-        && e.target!==this.nav.nativeElement && !this.nav.nativeElement.contains(e.target)
-        && e.target!==this.navButton.nativeElement && !this.navButton.nativeElement.contains(e.target)
-        &&e.target!==this.menuButton.nativeElement && !this.menuButton.nativeElement.contains(e.target)){
+      if(e.target!==this.menu?.nativeElement && !this.menu?.nativeElement.contains(e.target)
+        && e.target!==this.nav?.nativeElement && !this.nav?.nativeElement.contains(e.target)
+        && e.target!==this.navButton?.nativeElement && !this.navButton?.nativeElement.contains(e.target)
+        && e.target!==this.menuButton?.nativeElement && !this.menuButton?.nativeElement.contains(e.target)){
           this.menuActive=false;
           this.navActive=false;
       }
@@ -40,10 +53,25 @@ export class HeaderComponent implements OnInit, OnDestroy{
     
   }
 
-  toggleMenu () {
+  toggleMenu (): void {
     this.menuActive = !this.menuActive;
   }
-  toggleNav () {
+
+  toggleNav (): void {
     this.navActive = !this.navActive;
+  }
+
+  login(): void {
+    this.user.isAuthenticated = true;
+  }
+
+  signin(): void {
+    this.user.isAuthenticated = true;
+  }
+
+  signout(): void {
+    this.menuActive = false;
+    this.navActive = false;
+    this.user.isAuthenticated = false;
   }
 }
---
+++ src/app/models/user.ts
@@ -0,0 +1,7 @@
+export class User {
+    id?: number;
+    name?: string;
+    email?: string;
+    perfil?: string;
+    isAuthenticated?: boolean;
+}
