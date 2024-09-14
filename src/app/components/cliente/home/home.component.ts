69b6e747128b9c6b0034fa72d2343464ce492ec3
Viviane Mori <vivianeendomori@gmail.com>
2024-09-13 23:59:43 -0300

    add dynamic action button to order, styles

--- src/app/components/cliente/home/home.component.ts
+++ src/app/components/cliente/home/home.component.ts
@@ -1,5 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+export enum TipoEquipamento {
+  notebook = 'Notebook/laptop',
+  celular = 'Celular',
+  tablet = 'Tablet',
+  periferico = 'Periférico',
+  televisao = 'Televisão',
+  camera = 'Câmera',
+  drone = 'Drone',
+  videogameConsole = 'Console de videogame',
+  videogameAcessorio = 'Acessório de videogame',
+}
+
+export enum EstadoSolicitacao {
+  aberta = 'aberta',
+  orcada = 'orçada',
+  aprovada = 'aprovada',
+  rejeitada = 'rejeitada',
+  redirecionada = 'redirecionada',
+  arrumada = 'arrumada',
+  aguardandoPagamento = 'aguardando pagamento',
+  paga = 'paga',
+  finalizada = 'finalizada',
+}
+
+export interface Movimentacao {
+  dtHrMovimentacao: Date;
+  descricaoMovimentacao: EstadoSolicitacao;
+}
+
+export interface Solicitacao {
+  id: number;
+  equipamento: string;
+  descricaoEquipamento: string;
+  categoria: TipoEquipamento;
+  dtHrCriacao: Date;
+  descricaoProblema: string;
+  estadoAtual: EstadoSolicitacao;
+  historicoMovimentacao: Movimentacao[];
+}
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -13,15 +53,18 @@ export class HomeComponent {
       id: 1,
       equipamento: 'Notebook Dell Latitude 5450',
       descricaoEquipamento: 'Notebook pessoal, 512GB SSD, 32GB RAM, ano 2023',
-      categoria: 'Notebook/laptop',
-      dtHrCriacao: 'Thu Sep 12 2024 01:35:21 GMT-0300 (Brasilia Standard Time)',
+      categoria: TipoEquipamento.notebook,
+      dtHrCriacao: new Date(
+        'Thu Sep 12 2024 01:35:21 GMT-0300 (Brasilia Standard Time)'
+      ),
       descricaoProblema: 'Derrubou líquido, agora o notebook não liga mais.',
-      estadoAtual: 'aberta',
+      estadoAtual: EstadoSolicitacao.aberta,
       historicoMovimentacao: [
         {
-          dtHrMovimentacao:
-            'Thu Sep 12 2024 01:35:21 GMT-0300 (Brasilia Standard Time)',
-          descricaoMovimentacao: 'aberta',
+          dtHrMovimentacao: new Date(
+            'Thu Sep 12 2024 01:35:21 GMT-0300 (Brasilia Standard Time)'
+          ),
+          descricaoMovimentacao: EstadoSolicitacao.aberta,
         },
       ],
     },
@@ -29,20 +72,24 @@ export class HomeComponent {
       id: 2,
       equipamento: 'Celular iPhone 12',
       descricaoEquipamento: 'Celular pessoal, 128GB, ano 2021',
-      categoria: 'Celular',
-      dtHrCriacao: 'Mon Sep 09 2024 14:55:09 GMT-0300 (Brasilia Standard Time)',
+      categoria: TipoEquipamento.celular,
+      dtHrCriacao: new Date(
+        'Mon Sep 09 2024 14:55:09 GMT-0300 (Brasilia Standard Time)'
+      ),
       descricaoProblema: 'Tela quebrada.',
-      estadoAtual: 'orçada',
+      estadoAtual: EstadoSolicitacao.orcada,
       historicoMovimentacao: [
         {
-          dtHrMovimentacao:
-            'Mon Sep 09 2024 14:55:09 GMT-0300 (Brasilia Standard Time)',
-          descricaoMovimentacao: 'aberta',
+          dtHrMovimentacao: new Date(
+            'Mon Sep 09 2024 14:55:09 GMT-0300 (Brasilia Standard Time)'
+          ),
+          descricaoMovimentacao: EstadoSolicitacao.aberta,
         },
         {
-          dtHrMovimentacao:
-            'Wed Sep 11 2024 09:51:54 GMT-0300 (Brasilia Standard Time)',
-          descricaoMovimentacao: 'orçada',
+          dtHrMovimentacao: new Date(
+            'Wed Sep 11 2024 09:51:54 GMT-0300 (Brasilia Standard Time)'
+          ),
+          descricaoMovimentacao: EstadoSolicitacao.orcada,
         },
       ],
     },
@@ -50,25 +97,30 @@ export class HomeComponent {
       id: 3,
       equipamento: 'PlayStation 5',
       descricaoEquipamento: 'PlayStation 5 branco com controle DualSense',
-      categoria: 'Console de videogame',
-      dtHrCriacao: 'Mon Sep 09 2024 14:55:09 GMT-0300 (Brasilia Standard Time)',
+      categoria: TipoEquipamento.videogameConsole,
+      dtHrCriacao: new Date(
+        'Mon Sep 09 2024 14:55:09 GMT-0300 (Brasilia Standard Time)'
+      ),
       descricaoProblema: 'Conector de tomada com defeito.',
-      estadoAtual: 'aprovada',
+      estadoAtual: EstadoSolicitacao.aprovada,
       historicoMovimentacao: [
         {
-          dtHrMovimentacao:
-            'Mon Sep 09 2024 14:55:09 GMT-0300 (Brasilia Standard Time)',
-          descricaoMovimentacao: 'aberta',
+          dtHrMovimentacao: new Date(
+            'Mon Sep 09 2024 14:55:09 GMT-0300 (Brasilia Standard Time)'
+          ),
+          descricaoMovimentacao: EstadoSolicitacao.aberta,
         },
         {
-          dtHrMovimentacao:
-            'Wed Sep 11 2024 09:51:54 GMT-0300 (Brasilia Standard Time)',
-          descricaoMovimentacao: 'orçada',
+          dtHrMovimentacao: new Date(
+            'Wed Sep 11 2024 09:51:54 GMT-0300 (Brasilia Standard Time)'
+          ),
+          descricaoMovimentacao: EstadoSolicitacao.orcada,
         },
         {
-          dtHrMovimentacao:
-            'Wed Sep 12 2024 14:32:04 GMT-0300 (Brasilia Standard Time)',
-          descricaoMovimentacao: 'aprovada',
+          dtHrMovimentacao: new Date(
+            'Wed Sep 12 2024 14:32:04 GMT-0300 (Brasilia Standard Time)'
+          ),
+          descricaoMovimentacao: EstadoSolicitacao.aprovada,
         },
       ],
     },
@@ -76,27 +128,55 @@ export class HomeComponent {
       id: 4,
       equipamento: 'Samsung Galaxy S23',
       descricaoEquipamento: 'Samsung Galaxy S23 5G 256GB Preto',
-      categoria: 'Celular',
-      dtHrCriacao: 'Mon Sep 08 2024 12:09:12 GMT-0300 (Brasilia Standard Time)',
+      categoria: TipoEquipamento.celular,
+      dtHrCriacao: new Date(
+        'Mon Sep 08 2024 12:09:12 GMT-0300 (Brasilia Standard Time)'
+      ),
       descricaoProblema: 'Caiu na água e não liga mais.',
-      estadoAtual: 'rejeitada',
+      estadoAtual: EstadoSolicitacao.rejeitada,
       historicoMovimentacao: [
         {
-          dtHrMovimentacao:
-            'Wed Sep 08 2024 12:09:12 GMT-0300 (Brasilia Standard Time)',
-          descricaoMovimentacao: 'aberta',
+          dtHrMovimentacao: new Date(
+            'Wed Sep 08 2024 12:09:12 GMT-0300 (Brasilia Standard Time)'
+          ),
+          descricaoMovimentacao: EstadoSolicitacao.aberta,
         },
         {
-          dtHrMovimentacao:
-            'Wed Sep 11 2024 16:23:35 GMT-0300 (Brasilia Standard Time)',
-          descricaoMovimentacao: 'orçada',
+          dtHrMovimentacao: new Date(
+            'Wed Sep 11 2024 16:23:35 GMT-0300 (Brasilia Standard Time)'
+          ),
+          descricaoMovimentacao: EstadoSolicitacao.orcada,
         },
         {
-          dtHrMovimentacao:
-            'Mon Sep 12 2024 19:53:41 GMT-0300 (Brasilia Standard Time)',
-          descricaoMovimentacao: 'rejeitada',
+          dtHrMovimentacao: new Date(
+            'Mon Sep 12 2024 19:53:41 GMT-0300 (Brasilia Standard Time)'
+          ),
+          descricaoMovimentacao: EstadoSolicitacao.rejeitada,
         },
       ],
     },
   ];
+
+  shouldShowActionButton(solicitacao: Solicitacao): boolean {
+    return solicitacao.estadoAtual === EstadoSolicitacao.orcada ||
+      solicitacao.estadoAtual === EstadoSolicitacao.rejeitada ||
+      solicitacao.estadoAtual === EstadoSolicitacao.arrumada;
+  }
+
+  getActionText(solicitacao: Solicitacao): string {
+    switch (solicitacao.estadoAtual) {
+      case 'aberta':
+        return '';
+      case 'orçada':
+        return 'Aprovar ou rejeitar';
+      case 'aprovada':
+        return '';
+      case 'rejeitada':
+        return 'Resgatar';
+      case 'arrumada':
+        return 'Pagar';
+      default:
+        return '';
+    }
+  }
 }
