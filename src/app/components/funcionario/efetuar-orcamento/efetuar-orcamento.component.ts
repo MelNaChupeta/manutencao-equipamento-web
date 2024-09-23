3e247c1254f854e0adb82913868988a819bed2c6
rodolfo9254 <rodolfo9254@gmail.com>
2024-09-23 00:22:40 -0300

    RF012 efetuar orcamento - html done

--- src/app/components/funcionario/efetuar-orcamento/efetuar-orcamento.component.ts
+++ src/app/components/funcionario/efetuar-orcamento/efetuar-orcamento.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import mockOrcamento from './mockOrcamento.json';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { FormsModule } from '@angular/forms';
 
+export enum TipoEquipamento {
+  notebook = 'Notebook/laptop',
+  desktop = 'Desktop',
+  celular = 'Celular',
+  tablet = 'Tablet',
+  impressora = 'Impressora',
+  mouse = 'Mouse',
+  teclado = 'Teclado',
+  televisao = 'Televisão',
+  camera = 'Câmera',
+  drone = 'Drone',
+  videogameConsole = 'Console de videogame',
+  videogameAcessorio = 'Acessório de videogame',
+}
 
 @Component({
   selector: 'app-efetuar-orcamento',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgxMaskDirective, NgxMaskPipe, FormsModule],
+  providers: [provideNgxMask()],
+
   templateUrl: './efetuar-orcamento.component.html',
-  styleUrl: './efetuar-orcamento.component.scss'
+  styleUrl: './efetuar-orcamento.component.scss',
 })
 export class EfetuarOrcamentoComponent {
+  data = mockOrcamento;
 
-  data = mockOrcamento
-
+  valor: string = '';
 }
