1a42a0028b36ed1b8105fa14de63e4ff07f3d980
Viviane Mori <vivianeendomori@gmail.com>
2024-09-27 19:13:57 -0300

    fix: letra faltando

--- src/app/components/cliente/manter-solicitacao/manter-solicitacao.component.ts
+++ src/app/components/cliente/manter-solicitacao/manter-solicitacao.component.ts
@@ -4,13 +4,15 @@ import { ActivatedRoute } from '@angular/router';
 import { Solicitacao, solicitacoes, Movimentacao } from '../../../solicitacoes';
 import { NgOptimizedImage, registerLocaleData } from '@angular/common';
 import localePT from '@angular/common/locales/pt';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 
 registerLocaleData(localePT);
 
 @Component({
   selector: 'app-manter-solicitacao',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule, NgOptimizedImage, FontAwesomeModule],
   templateUrl: './manter-solicitacao.component.html',
   styleUrl: './manter-solicitacao.component.scss',
 })
@@ -18,6 +20,7 @@ export class ManterSolicitacaoComponent implements OnInit {
   solicitacao: Solicitacao | undefined;
   solicitacoes = solicitacoes;
   movimentacao: Movimentacao | undefined;
+  faCircleCheck = faCircleCheck;
   constructor(private route: ActivatedRoute) {}
   ngOnInit () {
     const routeParams = this.route.snapshot.paramMap;
