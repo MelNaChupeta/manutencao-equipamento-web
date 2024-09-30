06753dbcf2f6f5695c97b09882a8aaf34afa4a90
rodolfo9254 <rodolfo9254@gmail.com>
2024-09-29 22:05:15 -0300

    RF013 - html done, color ok and added actions buttons

--- src/app/components/funcionario/ver-solicitacoes/ver-solicitacoes.component.ts
+++ src/app/components/funcionario/ver-solicitacoes/ver-solicitacoes.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import mockVerSolicitacoes from './mockVerSolicitacoes.json';
+
+
 
 @Component({
   selector: 'app-ver-solicitacoes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ver-solicitacoes.component.html',
   styleUrl: './ver-solicitacoes.component.scss'
 })
 export class VerSolicitacoesComponent {
+  solicitacoes = mockVerSolicitacoes;
 
+  estadoAcoes: { [key: string]: string } = {
+    aberta: "Efetuar Orçamento",
+    aprovada: "Efetuar Manutenção",
+    redirecionada: "Efetuar Manutenção",
+    paga: "Finalizar Solicitação",
+  };
 }
