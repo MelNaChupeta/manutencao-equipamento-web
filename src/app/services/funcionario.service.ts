c8c8870b8e6823e038a6f2f3afeb7a7bbb416b7f
rodolfo9254 <rodolfo9254@gmail.com>
2024-10-14 12:57:16 -0300

    fix indentation

--- src/app/services/funcionario.service.ts
+++ src/app/services/funcionario.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { funcionario } from '../models/funcionario';
-import { User } from '../models/user';
+import { User } from '../models/user'
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from './user.service';
@@ -17,64 +17,62 @@ import mockOrcamento from '../mocks/mockOrcamento.json';
 import mockVerSolicitacoes from '../mocks/mockVerSolicitacoes.json';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class FuncionarioService {
-  LS_CHAVE = 'funcionarios';
 
+  LS_CHAVE = "funcionarios";
+  
   constructor(
     private router: Router,
     private http: HttpClient,
-    private userService: UserService
-  ) {}
+    private userService: UserService) {}
 
   private secretKey = new TextEncoder().encode('your-256-bit-secret');
-  url: string = '';
+  url:string = "";
 
-  funcionario(funcionario: funcionario) {
-    return this.http.post<funcionario>(`${this.url + 'auth'}`, { funcionario });
+  funcionario(funcionario: funcionario){
+    return this.http.post<funcionario>(`${this.url+ 'auth'}`, {funcionario});
   }
 
-  listarTodos(): funcionario[] {
+  listarTodos(): funcionario[]{
     const funcionarios = localStorage[this.LS_CHAVE];
 
-    return funcionarios ? JSON.parse(funcionarios) : [];
+    return funcionarios? JSON.parse(funcionarios) : [];
   }
 
-  inserir(funcionario: funcionario): void {
+  inserir(funcionario: funcionario): void{
     const funcionarios = this.listarTodos();
 
     funcionario.id = new Date().getTime();
 
     funcionarios.push(funcionario);
 
-    localStorage[this.LS_CHAVE] = JSON.stringify(funcionarios);
+    localStorage[this.LS_CHAVE] = JSON.stringify(funcionarios); 
   }
 
   buscarPorId(id: number): funcionario | undefined {
     const funcionarios = this.listarTodos();
 
-    return funcionarios.find((funcionario) => funcionario.id === id);
+    return funcionarios.find(funcionario => funcionario.id ===id);
   }
 
-  atualizar(funcionario: funcionario): void {
+  atualizar(funcionario: funcionario): void{
     const funcionarios = this.listarTodos();
 
-    funcionarios.forEach((obj, index, objs) => {
-      //obj: o elemento corrente do laço
-      if (funcionario.id === obj.id) {
-        //index: o índice do elemento corrente
-        objs[index] = funcionario; //objs: o vetor usado para a chamada de forEach
+    funcionarios.forEach((obj, index, objs) => { //obj: o elemento corrente do laço
+      if (funcionario.id === obj.id) {           //index: o índice do elemento corrente
+        objs[index] = funcionario                //objs: o vetor usado para a chamada de forEach
       }
     });
 
     localStorage[this.LS_CHAVE] = JSON.stringify(funcionarios);
   }
 
-  remover(id: number): void {
+  remover(id: number): void{
     let funcionarios = this.listarTodos();
 
-    funcionarios = funcionarios.filter((funcionario) => funcionario.id !== id);
+    funcionarios = funcionarios.filter(funcionario => funcionario.id !== id);
 
     localStorage[this.LS_CHAVE] = JSON.stringify(funcionarios);
   }
