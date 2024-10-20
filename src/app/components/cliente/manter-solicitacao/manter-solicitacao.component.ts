8e0a92d49f56ada88abb32af8699946ccde89f0a
Viviane Mori <vivianeendomori@gmail.com>
2024-10-19 23:07:54 -0300

    feat: adiciona botao acao cliente na solicitacao, etiqueta da situacao atual dinamica

--- src/app/components/cliente/manter-solicitacao/manter-solicitacao.component.ts
+++ src/app/components/cliente/manter-solicitacao/manter-solicitacao.component.ts
@@ -6,6 +6,7 @@ import { NgOptimizedImage, registerLocaleData } from '@angular/common';
 import localePT from '@angular/common/locales/pt';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
+import { EstadoSolicitacao } from '../../../models';
 
 registerLocaleData(localePT);
 
@@ -17,15 +18,75 @@ registerLocaleData(localePT);
   styleUrl: './manter-solicitacao.component.scss',
 })
 export class ManterSolicitacaoComponent implements OnInit {
-  solicitacao: Solicitacao | undefined;
+  solicitacao?: Solicitacao;
   solicitacoes = solicitacoes;
-  movimentacao: Movimentacao | undefined;
+  movimentacao?: Movimentacao;
   faCircleCheck = faCircleCheck;
   constructor(private route: ActivatedRoute) {}
-  ngOnInit () {
+
+  ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const idSolicitacaoFromRoute = routeParams.get('idSolicitacao');
-    this.solicitacao = solicitacoes.find(solicitacao => solicitacao.id === idSolicitacaoFromRoute);
-  };
-  imagemControle = "../../../../assets/controle.png";
+    this.solicitacao = solicitacoes.find(
+      (solicitacao) => solicitacao.id === idSolicitacaoFromRoute
+    );
+    if (!this.solicitacao) {
+      console.error(`Solicitação #${idSolicitacaoFromRoute} não encontrada!`);
+    }
+  }
+  imagemControle = '../../../../assets/controle.png';
+
+  getBotoesAcaoCliente(): { textoBotao: string; acao: () => void }[] {
+    if (!this.solicitacao) {
+      return [];
+    }
+    switch (this.solicitacao.estadoAtual) {
+      case EstadoSolicitacao.aberta:
+        return [];
+      case EstadoSolicitacao.orcada:
+        return [
+          {
+            textoBotao: 'Aprovar orçamento',
+            acao: () => this.aprovarOrcamento(),
+          },
+          { textoBotao: 'Rejeitar orçamento', acao: () => this.rejeitarOrcamento() },
+        ];
+      case EstadoSolicitacao.rejeitada:
+        return [
+          { textoBotao: 'Resgatar solicitação', acao: () => this.resgatarOrcamento() },
+        ];
+      case EstadoSolicitacao.aprovada:
+        return [];
+      case EstadoSolicitacao.aguardandoPagamento:
+        return [{ textoBotao: 'Pagar serviço', acao: () => this.pagarOrcamento() }];
+      case EstadoSolicitacao.paga:
+        return [];
+      default:
+        return [];
+    }
+  }
+
+  aprovarOrcamento() {
+    if (this.solicitacao) {
+      this.solicitacao.estadoAtual = 'aprovada' as EstadoSolicitacao;
+    }
+  }
+
+  rejeitarOrcamento() {
+    if (this.solicitacao) {
+      this.solicitacao.estadoAtual = 'rejeitada' as EstadoSolicitacao;
+    }
+  }
+
+  resgatarOrcamento() {
+    if (this.solicitacao) {
+      this.solicitacao.estadoAtual = 'orcada' as EstadoSolicitacao;
+    }
+  }
+
+  pagarOrcamento() {
+    if (this.solicitacao) {
+      this.solicitacao.estadoAtual = 'paga' as EstadoSolicitacao;
+    }
+  }
 }
